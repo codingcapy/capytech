05d35ee012d7cd7372dce64cb0d467ffc399f4c1
Paul <spkim0921@gmail.com>
2023-07-13 17:24:26 -0700

    49th commit
    bug fix main menu width > 700

--- script/menu.js
+++ script/menu.js
@@ -17,6 +17,9 @@ const $mainNav = $('#main-nav');
 const hamburgerMenu = document.getElementById('hamburger-menu');
 
 $mainNav.hide();
+if (window.innerWidth > 700){
+   $mainNav.show();
+}
 
 hamburgerMenu.addEventListener('click', function(){
 	$mainNav.slideToggle();
