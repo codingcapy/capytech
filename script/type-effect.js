4cc5252bc442cbfb0165e9db0678cf3e958e39aa
Paul K <spkim0921@gmail.com>
2024-05-03 11:24:32 -0700

    111th commit
    converting typing effect from css keyframes to js recursive functions to render each char

---
+++ script/type-effect.js
@@ -0,0 +1,20 @@
+
+
+
+const intro = document.querySelector(".intro");
+const myName = document.querySelector(".myName");
+const content = 'Hi, I\'m';
+const content2 = 'Paul👨🏻‍💻'
+
+function typingEffect(element, text, i = 0) {
+  element.innerHTML += text[i];
+  if (i === text.length - 1) {
+    return;
+  }
+  setTimeout(() => typingEffect(element, text, i + 1), 100)
+}
+
+typingEffect(intro, content);
+setTimeout(()=>typingEffect(myName, content2),1000)
+
+
